84d5d00d1c3849c7c4aa2dbc71a8b0d70f5e7a94
viacheslav <sslavian812@yandex.ru>
2015-12-01 11:26:33 +0300

    init

---
+++ src/main/java/ru/ifmo/ctddev/App.java
@@ -0,0 +1,13 @@
+package ru.ifmo.ctddev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
